ed7c5bac06038813c62e2c6a9e59d64ac132face
r17017790 <alexis.ravet@etu.univ-amu.fr>
2018-11-30 11:24:09 +0100

    BruteRasterImage tous les methodes ainsi que les constructeurs

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -2,6 +2,8 @@ package image;
 
 import javafx.scene.paint.Color;
 
+
+
 public class BruteRasterImage implements Image {
     private int width;
     private int height;
@@ -11,7 +13,7 @@ public class BruteRasterImage implements Image {
     public BruteRasterImage(Color color,int width, int height ) {
         this.width = width;
         this.height = height;
-
+            pixels = new Color[width][height];
         for(int x=0 ; x<width ; x++){
             for(int y=0 ; y<height ; y++){pixels[x][y]=color;
             }
@@ -20,23 +22,28 @@ public class BruteRasterImage implements Image {
 
     public BruteRasterImage(Color[][]colors){
         this.pixels = colors.clone();
+        this.width=colors.length;
+        this.height=colors[0].length;
     }
+
+
     public void createRepresentation(){
-        pixels= new Color[width][height];
-    }
-    @Override
-    public Color getPixelColor(int x, int y) {
-        return this.pixels[x][y]  ;
+        this.pixels= new Color[width][height];
     }
     public void setPixelColor(Color color , int x , int y){
         this.pixels[x][y] = color;
     }
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return this.pixels[x][y];
+    }
+
     public void setPixelsColor(Color[][] pixels){
-        this.pixels = pixels;
+        this.pixels= pixels;
     }
     public void setPixelsColor(Color color){
-        for(int x =0 ; x<width ; x++){
-            for(int y=0 ; y<height ; y++){
+        for(int x =0 ; x<this.width ; x++){
+            for(int y=0 ; y<this.height ; y++){
                 this.pixels[x][y]=color;
             }
         }
