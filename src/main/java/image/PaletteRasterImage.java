a2ac99ea907ecf21f5b93cda6ea03d4cab5010f1
r17017790 <alexis.ravet@etu.univ-amu.fr>
2018-12-05 12:02:43 +0100

    Commencement de la classe Palette

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -8,46 +8,55 @@ public class PaletteRasterImage implements Image {
     private int width;
     private int height;
     private List<Color> palette;
-    private byte[][] indexOfColors;
+    private int[][] indexOfColors;
 
     public PaletteRasterImage(Color color,int width, int height ) {
         this.width = width;
         this.height = height;
-        indexOfColors = new byte[width][height];
-        for(int x=0 ; x<width ; x++){
-            for(int y=0 ; y<height ; y++){indexOfColors[x][y] ;
+        for(int x=0;  x<width;x++){
+            for(int y=0;y<height;y++){
+
+                setPixelColor(color , x ,y);
+
             }
+
         };
     }
 
     public  PaletteRasterImage(Color[][]colors){
-        this.palette = colors.clone();
         this.width=colors.length;
         this.height=colors[0].length;
+        for(int index=0;index<=width;index++){
+            for(int index2=0;index<=height;index2++){
+                setPixelsColor(colors[index][index2]);
+            }
+        }
+
     }
 
 
     public void createRepresentation(){
-        this.indexOfColors= new Color[width][height];
+         = new Color[width][height];
     }
     public void setPixelColor(Color color , int x , int y){
-
-                if(palette=)
+        if(!palette.contains(color))
+            palette.add(indexOfColors[x][y] , color);
 
 
     }
     @Override
     public Color getPixelColor(int x, int y) {
-        return this.indexOfColors[x][y];
+        return 
     }
 
     public void setPixelsColor(Color[][] pixels){
-        this.indexOfColors= pixels;
+        palette.add(pixels[width][height]);
     }
     public void setPixelsColor(Color color){
         for(int x =0 ; x<this.width ; x++){
             for(int y=0 ; y<this.height ; y++){
-                this.indexOfColors[x][y]=color;
+                setPixelColor(color , x , y);
+
             }
         }
     }
